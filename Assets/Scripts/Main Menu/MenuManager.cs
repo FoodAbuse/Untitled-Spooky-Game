18989da3d47d1533d3129d0d6d8b9793b89d4bda
FoodAbuse <106326154+FoodAbuse@users.noreply.github.com>
2024-12-06 16:57:25 +0800

    Title Screen Testing
    Layout of title screen for functionality

---
+++ Assets/Scripts/Main Menu/MenuManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject[] toBeHidden;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            ProgressToTitle();
+        }
+    }
+
+    void ProgressToTitle()
+    {
+        foreach(GameObject g in toBeHidden)
+        {
+            g.SetActive(false);
+        }
+        
+    }
+}
