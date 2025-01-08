f9396109fd8689b4061ed6a1167cc2ecfd7771ee
SparkyAlduin <100830753+SparkyAlduin@users.noreply.github.com>
2025-01-08 18:51:57 +0800

    UpdateToMovementSystems
    I got rid of the first version of movement to place in animations and actually create a blend Tree. In the Process I took away all the old code

---
+++ Assets/MovementStates/States/WalkState.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkState : MovementBaseState
+{
+    public override void EnterState(MovementStateManager movement)
+    {
+      movement.anim.SetBool("Walking", true);
+    }
+
+    public override void UpdateState(MovementStateManager movement)
+    {
+        if(Input.GetKey(KeyCode.LeftShift)) ExitState(movement, movement.Run);
+        else if(Input.GetKeyDown(KeyCode.LeftControl)) ExitState(movement, movement.Crouch);
+        else if (movement.dir.magnitude < 0.1f) ExitState(movement, movement.Idle);
+    }
+
+    void ExitState(MovementStateManager movement, MovementBaseState state)
+    {
+        movement.anim.SetBool("Walking", false);
+        movement.SwitchState(state);
+    }
+}
