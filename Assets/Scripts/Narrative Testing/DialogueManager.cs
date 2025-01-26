4b617abbce355474e9e8d752f95af93ce55aac61
FoodAbuse <106326154+FoodAbuse@users.noreply.github.com>
2025-01-26 18:32:27 +0800

    Starting Dialogue Stuff
    poop

---
+++ Assets/Scripts/Narrative Testing/DialogueManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public void EnterDialogue()
+    {
+        //poop
+        
+    }
+}
